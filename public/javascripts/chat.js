a9ea41c2972c5214e4eb4ac8ec62ba0e4bcd6799
s-top <15634647@qq.com>
2017-09-29 17:01:33 +0800

    更新服务器

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,7 +5,7 @@
     var myName = false;
 
     //建立websocket连接
-    socket = io.connect('http://192.168.0.22:3000');
+    socket = io.connect('http://47.95.209.24:3000');
     //收到server的连接确认
     socket.on('open',function(){
         status.text('请输入用户名:');
